463fccf43419fdc36722e786c87b825a5beda51e
Jason <jasf0186@gmail.com>
2025-03-06 12:25:28 -0800

    Reviewed Basic Data Types

---
+++ CppCrashCourse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int age = 25;
+    age = 26;
+    float weight = 65.5;
+    double balance = 500000.34561;
+    char gender = 'm';
+    std::string username = "Code Jason";
+    bool isTodaySunny = false;
+    std::string colors[10] = {"red", "green", "pink", "blue", "black"};
+    std::cout << colors[0];
+    std::cin >> colors[0];
+    std::cout << colors[0];
+    colors[1] = "grey";
+
+}
